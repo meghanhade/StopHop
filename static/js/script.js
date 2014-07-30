ca98f1ccb74dfe12be3750a4df26d8cac6d315e5
Meghan Hade <mmhade@gmail.com>
2014-07-30 15:52:17 -0700

    multistop routing with time delay for intermediary stops

--- static/js/script.js
+++ static/js/script.js
@@ -45,7 +45,6 @@
       draw_route_1(data);
       var arrivalTime = data.plan.itineraries[0].endTime;
       var delayTime2 = $("#delayTime2").val();
-      console.log(delayTime2);
       get_route_2(arrivalTime, delayTime2);
     });
   }
@@ -62,6 +61,24 @@
           }));
         });
         draw_route_2(data2);
+        var arrivalTime = data2.plan.itineraries[0].endTime;
+        var delayTime3 = $("#delayTime2").val();
+        get_route_3(arrivalTime, delayTime3);
+    });
+  }
+
+  function get_route_3 (arrivalTime, delayTime3) {
+    if (delayTime3 === 'undefined'){
+      delayTime3 = 0;
+    }
+    var url3 = generate_url_3(arrivalTime, delayTime3);
+   $.getJSON(url3, function(data3) {
+        $.each(data3, function(index, element) {
+          $('body').append($('<div>', {
+            text: element.name
+          }));
+        });
+        draw_route_3(data3);
     });
   }
 
@@ -90,8 +107,8 @@
     if (delayTime2 === 'undefined'){
       delayTime2 = 0;
     }
-    var pointB = markerList[1].getLatLng();
-    var pointC = markerList[2].getLatLng();
+    var pointA = markerList[1].getLatLng();
+    var pointB = markerList[2].getLatLng();
     var inputTime = arrivalTime;
 
     var delay = parseInt(delayTime2) * 60 * 1000;
@@ -108,11 +125,36 @@
     var min = d.getMinutes();
 
 
-    url2 = "http:localhost:8080/otp/routers/default/plan?fromPlace="+pointB.lat+"%2C"+pointB.lng+"&toPlace="+pointC.lat+"%2C"+pointC.lng+"&mode=TRANSIT%2CWALK&maxWalkDistance=750&arriveBy=false&date="+year+"-"+month+"-"+day+"&time="+hour+":"+min;
+    url2 = "http:localhost:8080/otp/routers/default/plan?fromPlace="+pointA.lat+"%2C"+pointA.lng+"&toPlace="+pointB.lat+"%2C"+pointB.lng+"&mode=TRANSIT%2CWALK&maxWalkDistance=750&arriveBy=false&date="+year+"-"+month+"-"+day+"&time="+hour+":"+min;
     // console.log(url2)
-    console.log(inputTime);
-    console.log(delayedStart);
     return url2;
+  }
+
+  function generate_url_3 (arrivalTime, delayTime3) {
+    if (delayTime3 === 'undefined'){
+      delayTime3 = 0;
+    }
+    var pointA = markerList[2].getLatLng();
+    var pointB = markerList[3].getLatLng();
+    var inputTime = arrivalTime;
+
+    var delay = parseInt(delayTime3) * 60 * 1000;
+    var delayedStart = delay + inputTime;
+
+
+    var d = new Date(delayedStart);
+    var year = d.getFullYear();
+    //add one to month, as January is month 0:
+    var month = d.getMonth()+1;
+    var day = d.getDate();
+    //arrivalTime hours are already adjusted for GMT:
+    var hour = d.getHours();
+    var min = d.getMinutes();
+
+
+    url3 = "http:localhost:8080/otp/routers/default/plan?fromPlace="+pointA.lat+"%2C"+pointA.lng+"&toPlace="+pointB.lat+"%2C"+pointB.lng+"&mode=TRANSIT%2CWALK&maxWalkDistance=750&arriveBy=false&date="+year+"-"+month+"-"+day+"&time="+hour+":"+min;
+    // console.log(url2)
+    return url3;
   }
 
   // Generic URL generation Function :
@@ -207,4 +249,23 @@
       route_line.bindPopup("Mode: "+leg.mode+" ("+leg.routeShortName+") "+leg.routeLongName+". From: "+leg.from.name+". To: "+leg.to.name+". Depart at: "+startHour+":"+startMin+". Arrive by: "+endHour+":"+endMin);
     }
   }
+
+  function draw_route_3 (data3) {
+    var legs = data3.plan.itineraries[0].legs;
+
+    for(var i=0; i < legs.length; i++) {
+      var leg = legs[i];
+      var endTime = new Date(leg.endTime);
+      var startTime = new Date(leg.startTime);
+      var startHour = startTime.getHours();
+      var startMin = startTime.getMinutes();
+      var endHour = endTime.getHours();
+      var endMin = endTime.getMinutes();
+      // draw the polyline
+      var route_line = new L.Polyline(polyline.decode(leg.legGeometry.points)).addTo(pathLayer);
+      route_line.leg = leg;
+      route_line.bindPopup("Mode: "+leg.mode+" ("+leg.routeShortName+") "+leg.routeLongName+". From: "+leg.from.name+". To: "+leg.to.name+". Depart at: "+startHour+":"+startMin+". Arrive by: "+endHour+":"+endMin);
+    }
+  }
+
 })();
