21a9f17a4ae9b433c8f3f24610295c288b459e20
Meghan Hade <mmhade@gmail.com>
2014-07-30 15:30:54 -0700

    time delay working

--- static/js/script.js
+++ static/js/script.js
@@ -11,6 +11,7 @@
   var pathLayer = new L.LayerGroup().addTo(map);
 
   var markerCount = 1;
+  var markerList = [];
 
   function addMarker() {
     if (markerCount <= 4) {
@@ -20,8 +21,11 @@
     draggable: true
     }).addTo(map);
     markerCount += 1;
-    console.log("Count: ", markerCount);
-    console.log("Count string: ", markerCountString);
+    markerList.push(newMarker);
+    console.log(markerList);
+    for (marker in markerList) {
+      console.log(marker);
+    }
     return markerCount;
     }
     else {
@@ -40,12 +44,17 @@
       });
       draw_route_1(data);
       var arrivalTime = data.plan.itineraries[0].endTime;
-      get_route_2(arrivalTime);
+      var delayTime2 = $("#delayTime2").val();
+      console.log(delayTime2);
+      get_route_2(arrivalTime, delayTime2);
     });
   }
 
-  function get_route_2 (arrivalTime) {
-    var url2 = generate_url_2(arrivalTime);
+  function get_route_2 (arrivalTime, delayTime2) {
+    if (delayTime2 === 'undefined'){
+      delayTime2 = 0;
+    }
+    var url2 = generate_url_2(arrivalTime, delayTime2);
    $.getJSON(url2, function(data2) {
         $.each(data2, function(index, element) {
           $('body').append($('<div>', {
@@ -57,8 +66,11 @@
   }
 
    function generate_url(inputTime, delayTime) {
-    var pointA = marker.getLatLng();
-    var pointB = marker2.getLatLng();
+    if (delayTime === 'undefined'){
+      delayTime = 0;
+    }
+    var pointA = markerList[0].getLatLng();
+    var pointB = markerList[1].getLatLng();
     var inputTime = $("input#dateTime").val();
     var d = new Date(inputTime);
     var year = d.getFullYear();
@@ -74,14 +86,19 @@
     return url;
   }
 
-  function generate_url_2 (arrivalTime) {
-    var pointB = marker2.getLatLng();
-    var pointC = marker3.getLatLng();
+  function generate_url_2 (arrivalTime, delayTime2) {
+    if (delayTime2 === 'undefined'){
+      delayTime2 = 0;
+    }
+    var pointB = markerList[1].getLatLng();
+    var pointC = markerList[2].getLatLng();
     var inputTime = arrivalTime;
 
-    // var delay = ***INPUT FROM MINUTE FORM****
+    var delay = parseInt(delayTime2) * 60 * 1000;
+    var delayedStart = delay + inputTime;
 
-    var d = new Date(inputTime);
+
+    var d = new Date(delayedStart);
     var year = d.getFullYear();
     //add one to month, as January is month 0:
     var month = d.getMonth()+1;
@@ -90,8 +107,11 @@
     var hour = d.getHours();
     var min = d.getMinutes();
 
+
     url2 = "http:localhost:8080/otp/routers/default/plan?fromPlace="+pointB.lat+"%2C"+pointB.lng+"&toPlace="+pointC.lat+"%2C"+pointC.lng+"&mode=TRANSIT%2CWALK&maxWalkDistance=750&arriveBy=false&date="+year+"-"+month+"-"+day+"&time="+hour+":"+min;
     // console.log(url2)
+    console.log(inputTime);
+    console.log(delayedStart);
     return url2;
   }
 
@@ -127,8 +147,24 @@
   //   return url;
   // }
 
+  // function get_routes () {
+  //     for (var i=0; i < 4; i++) {
+  //       if (i === "2") {
+  //           // some jquery to retrieve input
+
+  //           generic_generate_url()//include delay)
+  //       } else if (i == "3") {
+
+  //       } else {
+  //         // don't pass in delay
+  //       }
+  //     }
+
+  // }
+
   $(document).ready(function () {
     $(".ui-button_route").click(get_route_1);
+    //$('.ui-button_route').click(get_routes);
   });
 
   $(document).ready(function () {
